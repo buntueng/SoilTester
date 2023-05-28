30348756d7e35f76faf3c8a4fe6a33b039972d2f
Buntueng Yana <mr.buntueng@gmail.com>
2023-05-28 13:33:33 +0700

    read limit switch

--- arduino_code/test_limit/test_limit.ino
+++ arduino_code/test_limit/test_limit.ino
@@ -10,10 +10,10 @@ const int y_motor_dir2 = 4;
 const int y_motor_speed_pin = 2;
 
 // limit switches
-const int x_limit_front = 18;
-const int x_limit_back = 16;
-const int y_limit_top = 26;
-const int y_limit_bottom = 20;
+const int x_limit_front_pin = 18;
+const int x_limit_back_pin = 16;
+const int y_limit_top_pin = 26;
+const int y_limit_bottom_pin = 20;
 
 // x move button
 const int x_forward_pin = 11;
@@ -30,7 +30,7 @@ unsigned long loadcell_timer = 0;
 
 bool run_machine = false;
 
-//=======================================================
+//================= switch control inputs ==================
 bool x_backward_logic = 0;
 bool x_forward_logic = 0;
 bool y_down_logic = 0;
@@ -40,6 +40,17 @@ unsigned char x_backward_state = 0xFF;
 unsigned char x_forward_state = 0xFF;
 unsigned char y_down_state = 0xFF;
 unsigned char y_up_state = 0xFF;
+
+// ================= limit switch ======================
+bool x_limit_front_logic = 0;
+bool x_limit_back_logic = 0;
+bool y_limit_top_logic = 0;
+bool y_limit_bottom_logic = 0;
+
+unsigned char x_limit_front_state = 0xFF;
+unsigned char x_limit_back_state = 0xFF;
+unsigned char y_limit_top_state = 0xFF;
+unsigned char y_limit_bottom_state = 0xFF;
 //=======================================================
 
 void setup()
@@ -54,16 +65,16 @@ void setup()
     pinMode(y_motor_dir2,OUTPUT);
     pinMode(y_motor_speed_pin,OUTPUT);
 
-    pinMode(x_limit_front,INPUT_PULLUP);
-    pinMode(x_limit_back,INPUT_PULLUP);
+    pinMode(x_limit_front_pin,INPUT_PULLUP);
+    pinMode(x_limit_back_pin,INPUT_PULLUP);
 
     pinMode(x_forward_pin,INPUT_PULLUP);
     pinMode(x_backward_pin,INPUT_PULLUP);
 
     pinMode(y_up_pin,INPUT_PULLUP);
     pinMode(y_down_pin,INPUT_PULLUP);
-    pinMode(y_limit_top,INPUT_PULLUP);
-    pinMode(y_limit_bottom,INPUT_PULLUP);
+    pinMode(y_limit_top_pin,INPUT_PULLUP);
+    pinMode(y_limit_bottom_pin,INPUT_PULLUP);
 
     pinMode(x_loadcell_pin,INPUT);
     pinMode(y_loadcell_pin,INPUT);
@@ -80,54 +91,8 @@ void setup()
 
 void loop()
 {
-  // read all button logic
-  // Serial.print(digitalRead(x_forward_pin));
-  // Serial.print((","));
-  // Serial.print(digitalRead(x_backward_pin));
-  // Serial.print((","));
-  // Serial.print(digitalRead(y_up_pin));
-  // Serial.print((","));
-  // Serial.println(digitalRead(y_down_pin));
-  //////////////////// ============================= push = 0 ================================================
-  x_backward_state = ((x_backward_state << 1) + digitalRead(x_backward_pin)) & 0xFF;
-  if(x_backward_state == 0xFC)
-  {
-    x_backward_logic = 1;
-  }
-  else if(x_backward_state == 0x3F)
-  {
-    x_backward_logic = 0;
-  }
-
-  x_forward_state = ((x_forward_state << 1) + digitalRead(x_forward_pin)) & 0xFF;
-  if(x_forward_state == 0xFC)
-  {
-    x_forward_logic = 1;
-  }
-  else if(x_forward_state == 0x3F)
-  {
-    x_forward_logic = 0;
-  }
-
-  y_up_state = ((y_up_state << 1) + digitalRead(y_up_pin)) & 0xFF;
-  if(y_up_state == 0xFC)
-  {
-    y_up_logic = 1;
-  }
-  else if(y_up_state == 0x3F)
-  {
-    y_up_logic = 0;
-  }
-
-  y_down_state = ((y_down_state << 1) + digitalRead(y_down_pin)) & 0xFF;
-  if(y_down_state == 0xFC)
-  {
-    y_down_logic = 1;
-  }
-  else if(y_down_state == 0x3F)
-  {
-    y_down_logic = 0;
-  }
+  scan_input_switches();
+  scan_limit_switches();
   //////////////////// =====================================================================================
 
   if(Serial.available())
@@ -196,32 +161,34 @@ void loop()
   }
   else
   {
-    Serial.print(x_forward_logic);
+    Serial.print(x_limit_front_logic);
     Serial.print(",");
-    Serial.print(x_backward_logic);
+    Serial.print(x_limit_back_logic);
     Serial.print(",");
-    Serial.print(y_up_logic);
+    Serial.print(y_limit_top_logic);
     Serial.print(",");
-    Serial.println(y_down_logic);
-    if(((x_forward_logic) == 0) && (digitalRead(x_limit_front)==0))
+    Serial.println(y_limit_bottom_logic);
+
+    
+    if(((x_forward_logic) == 0) && (digitalRead(x_limit_front_pin)==0))
     {
             digitalWrite(x_motor_dir1,HIGH);
             digitalWrite(x_motor_dir2,LOW);
             analogWrite(x_motor_speed_pin,50);      
     }
-    else if (((x_backward_logic)==0) && (digitalRead(x_limit_back)==0)) 
+    else if (((x_backward_logic)==0) && (digitalRead(x_limit_back_pin)==0)) 
     {
             digitalWrite(x_motor_dir1,LOW);
             digitalWrite(x_motor_dir2,HIGH);
             analogWrite(x_motor_speed_pin,50);
     }
-    else if (((y_up_logic)==0)&&(digitalRead(y_limit_top)==0))
+    else if (((y_up_logic)==0)&&(digitalRead(y_limit_top_pin)==0))
     {
             digitalWrite(y_motor_dir1,HIGH);
             digitalWrite(y_motor_dir2,LOW);
             analogWrite(y_motor_speed_pin,150);    
     }
-    else if(((y_down_logic)==0)&&(digitalRead(y_limit_bottom)==0))
+    else if(((y_down_logic)==0)&&(digitalRead(y_limit_bottom_pin)==0))
     {
             digitalWrite(y_motor_dir1,LOW);
             digitalWrite(y_motor_dir2,HIGH);
@@ -243,4 +210,94 @@ void loop()
       //       analogWrite(y_motor_speed_pin,0);
       //   }
   }    
+}
+
+
+// ====================== sub program ===============================
+void scan_input_switches()
+{
+  x_backward_state = ((x_backward_state << 1) + digitalRead(x_backward_pin)) & 0xFF;
+  if(x_backward_state == 0xFC)
+  {
+    x_backward_logic = 1;
+  }
+  else if(x_backward_state == 0x3F)
+  {
+    x_backward_logic = 0;
+  }
+
+  x_forward_state = ((x_forward_state << 1) + digitalRead(x_forward_pin)) & 0xFF;
+  if(x_forward_state == 0xFC)
+  {
+    x_forward_logic = 1;
+  }
+  else if(x_forward_state == 0x3F)
+  {
+    x_forward_logic = 0;
+  }
+
+  y_up_state = ((y_up_state << 1) + digitalRead(y_up_pin)) & 0xFF;
+  if(y_up_state == 0xFC)
+  {
+    y_up_logic = 1;
+  }
+  else if(y_up_state == 0x3F)
+  {
+    y_up_logic = 0;
+  }
+
+  y_down_state = ((y_down_state << 1) + digitalRead(y_down_pin)) & 0xFF;
+  if(y_down_state == 0xFC)
+  {
+    y_down_logic = 1;
+  }
+  else if(y_down_state == 0x3F)
+  {
+    y_down_logic = 0;
+  }
+}
+
+void scan_limit_switches()
+{
+  x_limit_front_state = ((x_limit_front_state << 1) + digitalRead(x_limit_front_pin)) & 0xFF;
+  if(x_limit_front_state == 0xFC)
+  {
+    x_limit_front_logic = 1;
+  }
+  else if(x_limit_front_state == 0x3F)
+  {
+    x_limit_front_logic = 0;
+  }
+
+  x_limit_back_state = ((x_limit_back_state << 1) + digitalRead(x_limit_back_pin)) & 0xFF;
+  if(x_limit_back_state == 0xFC)
+  {
+    x_limit_back_logic = 1;
+  }
+  else if(x_limit_back_state == 0x3F)
+  {
+    x_limit_back_logic = 0;
+  }
+
+  y_limit_top_state = ((y_limit_top_state << 1) + digitalRead(y_limit_top_pin)) & 0xFF;
+  if(y_limit_top_state == 0xFC)
+  {
+    y_limit_top_logic = 1;
+  }
+  else if(y_limit_top_state == 0x3F)
+  {
+    y_limit_top_logic = 0;
+  }
+
+  y_limit_bottom_state = ((y_limit_bottom_state << 1) + digitalRead(y_limit_bottom_pin)) & 0xFF;
+  if(y_limit_bottom_state == 0xFC)
+  {
+    y_limit_bottom_logic = 1;
+  }
+  else if(y_limit_bottom_state == 0x3F)
+  {
+    y_limit_bottom_logic = 0;
+  }
+
+
 }
